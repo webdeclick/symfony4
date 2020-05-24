b934b2e4f4290e0f5925e4242d7f07e7e4af5b03
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-24 14:03:58 +0200

    template propertycontroller

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PropertyController {
+class PropertyController extends AbstractController{
 
   /**
    * @Route("/biens", name="property.index")
    * @return Response
    */
   public function index(): Response{
-    return new Response('Les biens');
+    return $this->render('property/index.html.twig');
   }
 
 }
