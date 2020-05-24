d9bc032ef405646dd6d2e5897b6dec2515420035
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-24 13:54:44 +0200

    utilisation AbstractController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,30 +1,19 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController {
-
-  /**
-   * Undocumented variable
-   *
-   * @var Environment
-   */
-  private $twig;
-
-  public function __construct(Environment $twig)
-  {
-    $this->twig = $twig;
-  }
+class HomeController extends AbstractController {
 
   /**
    * @Route("/", name="home")
    * @return Response
    */
   public function index(): Response {
-    return new Response($this->twig->render('pages/home.html.twig'));
+    return $this->render('pages/home.html.twig');
   }
 
 }
