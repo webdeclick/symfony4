c759f405d6a1255ae67a14229631f46089a479cb
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-25 00:51:58 +0200

    creation repository avec queryBuilder

--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Property;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method Property|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,36 @@ class PropertyRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Property::class);
+    }
+    
+    /**
+     * @return Property[]
+     */
+    public function findAllVisible():array{
+        return $this->findVisibleQuery()
+            ->getQuery()
+            ->getResult();
+        ;
+    }
+
+    /**
+     * @return Property[]
+     */
+    public function findLatest():array{
+        return $this->findVisibleQuery()
+            ->setMaxResults(4)
+            ->getQuery()
+            ->getResult();
+        ;
+    }
+
+    /**
+     * @return Property[]
+     */
+    private function findVisibleQuery():QueryBuilder{
+        return $this->createQueryBuilder('p')
+            ->where('p.sold = false');
+        ;
     }
 
     // /**
