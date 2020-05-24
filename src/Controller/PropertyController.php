baf93dff50251d6a0d4bab35b8ba9e5171f95db5
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-24 15:18:42 +0200

    insert BD avec getDoctrine()->getManager()

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,21 @@ class PropertyController extends AbstractController{
    * @return Response
    */
   public function index(): Response{
+    $property = new Property;
+    $property->setTitle('Mon premier bien')
+    ->setPrice(200000)
+    ->setRooms(4)
+    ->setBedrooms(3)
+    ->setDescription('Une petite description')
+    ->setSurface(60)
+    ->setFloor(4)
+    ->setHeat(1)
+    ->setCity('Colmar')
+    ->setAddress('rue du test')
+    ->setPostalCode('68000');
+    $em=$this->getDoctrine()->getManager();
+    $em->persist($property);
+    $em->flush();
     return $this->render('property/index.html.twig', [
       'current_menu'=> 'properties'
     ]);
