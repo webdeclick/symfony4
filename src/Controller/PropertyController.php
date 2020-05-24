c1f30c3afabfe7e16cbc4967562486809a1a44a2
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-24 15:23:31 +0200

    recuperation repository via doctrine

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -14,21 +14,7 @@ class PropertyController extends AbstractController{
    * @return Response
    */
   public function index(): Response{
-    $property = new Property;
-    $property->setTitle('Mon premier bien')
-    ->setPrice(200000)
-    ->setRooms(4)
-    ->setBedrooms(3)
-    ->setDescription('Une petite description')
-    ->setSurface(60)
-    ->setFloor(4)
-    ->setHeat(1)
-    ->setCity('Colmar')
-    ->setAddress('rue du test')
-    ->setPostalCode('68000');
-    $em=$this->getDoctrine()->getManager();
-    $em->persist($property);
-    $em->flush();
+    $repository = $this->getDoctrine()->getRepository(Property::class);
     return $this->render('property/index.html.twig', [
       'current_menu'=> 'properties'
     ]);
