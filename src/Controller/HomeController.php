20db06602fd61391afd1d7fb98fcab78b1b484f3
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-25 00:53:29 +0200

    Autowiring PropertyRepository, passage des resultats à la vue

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +13,11 @@ class HomeController extends AbstractController {
    * @Route("/", name="home")
    * @return Response
    */
-  public function index(): Response {
-    return $this->render('pages/home.html.twig');
+  public function index(PropertyRepository $repository): Response {
+    $properties = $repository->findLatest();
+    return $this->render('pages/home.html.twig', [
+      'properties' => $properties
+    ]);
   }
 
 }
