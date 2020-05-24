41361fbb203797d68f4205016ef94395d849433e
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-25 00:49:39 +0200

    Autowiring PropertyRepository et EntityManagerInterface dans le constructeur du controller

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,18 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Property;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PropertyRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController{
 
   /**
+   * @var $repository
+   */
+  private $repository;
+
+  /**
+   * @var ObjectManager
+   */
+  private $em;
+
+  public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+  {
+    $this->repository = $repository;
+    $this->em = $em;
+  }
+  /**
    * @Route("/biens", name="property.index")
    * @return Response
    */
-  public function index(): Response{
-    $repository = $this->getDoctrine()->getRepository(Property::class);
+  public function index(){
     return $this->render('property/index.html.twig', [
       'current_menu'=> 'properties'
     ]);
