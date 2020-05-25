39d0011d7cdbf2063e3a03706c1a196d758885a2
de-moor.vincent <vincent.demoor68@gmail.com>
2020-05-25 22:37:45 +0200

    Utilisation de l'injection

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -41,8 +41,13 @@ class PropertyController extends AbstractController{
    * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug":"[a-z0-9\-]*"})
    * @return Response
    */
-  public function show($id, $slug){
-    $property = $this->repository->find($id);
+  public function show(Property $property, string $slug){
+    if($property->getSlug() !== $slug){
+      return $this->redirectToRoute('property.show', [
+        'id' => $property->getId(),
+        'slug' => $property->getSlug()
+      ], 301);
+    }
     return $this->render('property/show.html.twig', [
       'property' => $property,
       'current_menu'=> 'properties'
